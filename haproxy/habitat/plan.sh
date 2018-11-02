d648ba1c3ef6e35971c47d7707fca9166b5de4c2
Jonathan Tron <jonathan@tron.name>
2018-11-02 18:14:42 +0100

    Update HAProxy to 1.8.14

--- haproxy/habitat/plan.sh
+++ haproxy/habitat/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=jonathantron
 pkg_name=haproxy
 pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
-pkg_version=1.8.13
+pkg_version=1.8.14
 pkg_maintainer='Jonathan Tron <jonathan@tron.name>'
 pkg_license=('GPL-2.0' 'LGPL-2.1')
 pkg_source=http://www.haproxy.org/download/1.8/src/haproxy-${pkg_version}.tar.gz
 pkg_upstream_url="http://git.haproxy.org/git/haproxy-1.8.git/"
-pkg_shasum=2bf5dafbb5f1530c0e67ab63666565de948591f8e0ee2a1d3c84c45e738220f1
+pkg_shasum=b17e402578be85e58af7a3eac99b1f675953bea9f67af2e964cf8bdbd1bd3fdf
 pkg_svc_run='haproxy -f config/haproxy.conf -db'
 pkg_deps=(core/zlib core/pcre core/openssl)
 pkg_build_deps=(
