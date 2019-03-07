e6468814526268393ca3970abad0280779c207ce
Jonathan Tron <jonathan@tron.name>
2019-03-07 08:52:41 +0100

    Update prometheus-haproxy_exporter to 0.10.0

--- prometheus-haproxy_exporter/habitat/plan.sh
+++ prometheus-haproxy_exporter/habitat/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=prometheus-haproxy_exporter
 pkg_origin=jonathantron
-pkg_version="0.8.0"
+pkg_version="0.10.0"
 pkg_maintainer="Jonathan Tron <jonathan@tron.name>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/prometheus/haproxy_exporter/releases/download/v${pkg_version}/haproxy_exporter-${pkg_version}.linux-amd64.tar.gz"
 pkg_dirname="haproxy_exporter-${pkg_version}.linux-amd64"
-pkg_shasum="2b1da4218fc5a1531ed17663ba5656c6bb5ce3db0ad6c2bdd6781d7f1b545816"
+pkg_shasum="08150728e281f813a8fcfff4b336f16dbfe4268a1c7510212c8cff2579b10468"
 pkg_deps=()
 pkg_build_deps=()
 pkg_bin_dirs=(bin)
