70be6f2e09e4c79d146a7275b2ebd052a5bc683d
Jonathan Tron <jonathan@tron.name>
2019-11-25 14:33:17 +0100

    Update haproxy2 to 2.0.9

--- haproxy2/habitat/plan.sh
+++ haproxy2/habitat/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=jonathantron
 pkg_name=haproxy2
 pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
-pkg_version=2.0.7
+pkg_version=2.0.9
 pkg_maintainer='Jonathan Tron <jonathan@tron.name>'
 pkg_license=('GPL-2.0' 'LGPL-2.1')
 pkg_source=http://www.haproxy.org/download/2.0/src/haproxy-${pkg_version}.tar.gz
 pkg_upstream_url="http://git.haproxy.org/git/haproxy-2.0.git/"
 pkg_dirname="haproxy-${pkg_version}"
-pkg_shasum=3873cd72028ed1bd2506dd174e01a92620e92683092f34234c96e067dcb113dc
+pkg_shasum=35692801abfd6dde4976cb42fe5cee8aaf61959e743003426073c3141494c589
 pkg_svc_run='haproxy -f config/haproxy.conf -db'
 pkg_deps=(core/zlib core/pcre jonathantron/openssl)
 pkg_build_deps=(
