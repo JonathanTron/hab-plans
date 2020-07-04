30eeadf448db31443d9e092cce4e6f87ee287d36
Jonathan Tron <jonathan@tron.name>
2020-07-04 12:44:37 +0200

    Update haproxy2 to 2.1.7 and beamium to 2.0.7

--- beamium/habitat/plan.sh
+++ beamium/habitat/plan.sh
@@ -1,27 +1,62 @@
 pkg_name=beamium
 pkg_origin=jonathantron
-pkg_version="1.9.3"
+pkg_version="2.0.7"
 pkg_maintainer="Jonathan Tron <jonathan@tron.name>"
 pkg_license=('BSD-3-Clause')
-pkg_source="https://github.com/ovh/beamium/archive/v${pkg_version}.tar.gz"
-pkg_shasum="18ff56b7ad7bc953bdcaa69fd69b7db071e32df2cefd36a088d15f2e02322f3f"
-pkg_deps=(core/gcc-libs core/glibc core/openssl core/cacerts)
-pkg_build_deps=(core/rust core/gcc core/gcc-libs core/glibc core/openssl core/cacerts core/git)
+pkg_source="https://github.com/ovh/beamium.git"
+pkg_shasum=noshasum
+pkg_deps=(
+  core/gcc-libs
+  core/glibc
+  core/openssl
+  core/cacerts
+)
+pkg_build_deps=(
+  core/rust
+  core/gcc
+  core/gcc-libs
+  core/glibc
+  core/openssl
+  core/cacerts
+  core/git
+)
 pkg_bin_dirs=(bin)
 
-do_prepare() {
+do_setup_environment() {
   # Used by Cargo to use a pristine, isolated directory for all compilation
   # From: https://github.com/habitat-sh/habitat/blob/8b672dd39c28deb2060bf93c2ca59d574d8a0390/support/ci/builder-base-plan.sh
-  export CARGO_TARGET_DIR="$HAB_CACHE_SRC_PATH/$pkg_dirname"
-  build_line "Setting CARGO_TARGET_DIR=$CARGO_TARGET_DIR"
-
+  set_buildtime_env CARGO_TARGET_DIR "$HAB_CACHE_SRC_PATH/$pkg_dirname"
   # Ensure ssl certs are found by rust lib
-  export SSL_CERT_FILE="{{pkg_path_for cacerts}}/ssl/cert.pem"
-  build_line "Setting SSL_CERT_FILE=$SSL_CERT_FILE"
-
+  set_buildtime_env SSL_CERT_FILE "$(pkg_path_for cacerts)/ssl/cert.pem"
   # Used to compile cargo deps based on openssl
-  export OPENSSL_DIR=$(pkg_path_for openssl)
-  build_line "Setting OPENSSL_DIR=$OPENSSL_DIR"
+  set_buildtime_env OPENSSL_DIR "$(pkg_path_for openssl)"
+}
+
+do_download() {
+  GIT_SSL_CAINFO="$(pkg_path_for core/cacerts)/ssl/certs/cacert.pem"
+  export GIT_SSL_CAINFO
+
+  REPO_PATH="$HAB_CACHE_SRC_PATH/$pkg_dirname"
+
+  # removing any previous git repo under the same package name that was downloaded
+  rm -rf "$REPO_PATH"
+
+  git clone "$pkg_source" "$REPO_PATH"
+
+  pushd "$REPO_PATH" || exit 1
+  git checkout "tags/v${pkg_version}"
+}
+
+do_clean() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_verify() {
+  return 0
 }
 
 do_build() {
--- haproxy2/habitat/plan.sh
+++ haproxy2/habitat/plan.sh
@@ -1,15 +1,19 @@
 pkg_origin=jonathantron
 pkg_name=haproxy2
 pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
-pkg_version=2.0.14
+pkg_version=2.1.7
 pkg_maintainer='Jonathan Tron <jonathan@tron.name>'
 pkg_license=('GPL-2.0' 'LGPL-2.1')
-pkg_source=http://www.haproxy.org/download/2.0/src/haproxy-${pkg_version}.tar.gz
-pkg_upstream_url="http://git.haproxy.org/git/haproxy-2.0.git/"
+pkg_source=http://www.haproxy.org/download/2.1/src/haproxy-${pkg_version}.tar.gz
+pkg_upstream_url="http://git.haproxy.org/git/haproxy-2.1.git/"
 pkg_dirname="haproxy-${pkg_version}"
-pkg_shasum=552a708b8b6efd0f241f5d9fd7ad4168d37ce17cdb6dcb6239c2e519f0a63c75
+pkg_shasum=392e6cf18e75fe7e166102e8c4512942890a0b5ae738f6064faab4687f60a339
 pkg_svc_run='haproxy -f config/haproxy.conf -db'
-pkg_deps=(core/zlib core/pcre jonathantron/openssl)
+pkg_deps=(
+  core/zlib
+  core/pcre
+  jonathantron/openssl
+)
 pkg_build_deps=(
   core/coreutils
   core/gcc
