c22e2691c27eb9a01954f20fea91b836906a7550
Jonathan Tron <jonathan@tron.name>
2021-08-02 09:54:08 +0200

    Update prometheus-node_exporter to 1.2.0

--- prometheus-node_exporter/habitat/plan.sh
+++ prometheus-node_exporter/habitat/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=prometheus-node_exporter
 pkg_origin=jonathantron
-pkg_version="0.15.1"
+pkg_version="1.2.0"
 pkg_maintainer="Jonathan Tron <jonathan@tron.name>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/prometheus/node_exporter/releases/download/v${pkg_version}/node_exporter-${pkg_version}.linux-amd64.tar.gz"
 pkg_dirname="node_exporter-${pkg_version}.linux-amd64"
-pkg_shasum="7ffb3773abb71dd2b2119c5f6a7a0dbca0cff34b24b2ced9e01d9897df61a127"
+pkg_shasum="f7ef26fb10d143dc4211281d7a2e8b13c4fe1bd0d7abbdff6735a6efdb4b5e56"
 pkg_deps=()
 pkg_build_deps=()
 pkg_bin_dirs=(bin)
