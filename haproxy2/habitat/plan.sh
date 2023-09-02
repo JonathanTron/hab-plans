fd8b9cb3351894cc71ee29a48ff5d1aba43a7037
Jonathan Tron <jonathan@tron.name>
2023-09-02 10:42:57 +0200

    Update haproxy2 to 2.8.2

--- haproxy2/habitat/plan.sh
+++ haproxy2/habitat/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=jonathantron
 pkg_name=haproxy2
 pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
-pkg_version=2.6.12
+pkg_version=2.8.2
 pkg_maintainer='Jonathan Tron <jonathan@tron.name>'
 pkg_license=('GPL-2.0' 'LGPL-2.1')
-pkg_source=http://www.haproxy.org/download/2.6/src/haproxy-${pkg_version}.tar.gz
-pkg_upstream_url="http://git.haproxy.org/git/haproxy-2.6.git/"
+pkg_source=http://www.haproxy.org/download/2.8/src/haproxy-${pkg_version}.tar.gz
+pkg_upstream_url="http://git.haproxy.org/git/haproxy-2.8.git/"
 pkg_dirname="haproxy-${pkg_version}"
-pkg_shasum=58f9edb26bf3288f4b502658399281cc5d6478468bd178eafe579c8f41895854
+pkg_shasum=698d6906d170946a869769964e57816ba3da3adf61ff75e89972b137f4658db0
 pkg_svc_run='haproxy -f config/haproxy.conf -db'
 pkg_deps=(
   core/zlib
